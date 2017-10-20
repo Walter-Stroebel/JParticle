7d9d2f8d5925a3857474f89635d1924e2050ea44
Walter-Stroebel <7278535+Walter-Stroebel@users.noreply.github.com>
2017-10-20 14:47:31 +0200

    Small changes.

--- src/main/java/nl/infcomtec/jparticle/DevicePoll.java
+++ src/main/java/nl/infcomtec/jparticle/DevicePoll.java
@@ -7,11 +7,20 @@ package nl.infcomtec.jparticle;
 import java.util.UUID;
 
 /**
- *
+ * Use this interface to create a poller.
+ * 
  * @author walter
  */
 public interface DevicePoll extends Runnable {
 
+    /** Should return the time in milliseconds to wait between polls.
+     * 
+     * @return The time in milliseconds between polls. 
+     */
     public long interval();
+    /**
+     * Unique identifier for this task.
+     * @return Should always return the same UUID for a task.
+     */
     public UUID uuid();
 }
