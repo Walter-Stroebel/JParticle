694f87e4e264896f1a263e1724d468e9977cdd35
Walter-Stroebel <7278535+Walter-Stroebel@users.noreply.github.com>
2017-10-20 14:53:02 +0200

    Small changes.

--- src/main/java/nl/infcomtec/jparticle/DeviceEvent.java
+++ src/main/java/nl/infcomtec/jparticle/DeviceEvent.java
@@ -8,26 +8,35 @@ import java.util.UUID;
 
 /**
  * Implement this interface to receive publish events.
+ *
  * @author walter
  */
 public interface DeviceEvent {
-    /** Called whenever a subscribed event arrives.
-     * 
-     * @param e The event from the cloud. 
+
+    /**
+     * Called whenever a subscribed event arrives.
+     *
+     * @param e The event from the cloud.
      */
     public void event(Event e);
-    /** Used to cancel a subscription.
-     * 
-     * @return Should always return the same UUID for a given task. 
+
+    /**
+     * Used to cancel a subscription.
+     *
+     * @return Should always return the same UUID for a given task.
      */
     public UUID uuid();
+
     /**
      * If not null, we want events for this device Id.
+     *
      * @return null or deviceId to match.
      */
     public String forDeviceId();
+
     /**
      * If not null, we want events for this device name.
+     *
      * @return null or device name to match.
      */
     public String forDeviceName();
